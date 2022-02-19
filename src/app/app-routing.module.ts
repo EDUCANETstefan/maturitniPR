dc95a88babca0344ca165eeec138ff8ae13713ba
Stefan <44003786+EDUCANETstefan@users.noreply.github.com>
2022-02-19 14:05:24 +0100

    error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import {HomeComponent} from "./home/home.component";
 import {ForumComponent} from "./forum/forum.component";
 import {ClankyComponent} from "./clanky/clanky.component";
 import {AboutComponent} from "./about/about.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'forum', component: ForumComponent},
   {path: 'clanky', component: ClankyComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
